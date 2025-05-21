64a9cd99ed4e6e3eebebb1472c121cc9c85135bf
Luke Charlesworth <lukejames4@gmail.com>
2025-05-21 17:11:12 -0400

    style sdk

--- src/components/VideonestEmbed.tsx
+++ src/components/VideonestEmbed.tsx
@@ -25,8 +25,6 @@ const VideonestEmbed: React.FC<VideonestEmbedProps> = ({ videoId, config, style 
   if (primaryColor) params.push(`primary_color=${primaryColor.replace('#', '')}`);
   if (secondaryColor) params.push(`secondary_color=${secondaryColor.replace('#', '')}`);
   if (darkMode) params.push('dark_mode=true');
-  if (width) params.push(`width=${width}`);
-  if (height) params.push(`height=${height}`);
   if (showTitle) params.push('show_title=true');
   if (showDescription) params.push('show_description=true');
   
@@ -39,29 +37,14 @@ const VideonestEmbed: React.FC<VideonestEmbedProps> = ({ videoId, config, style 
   }
 
   return (
-    <div
-      style={{
-        position: 'relative',
-        width: style.width || '100%',
-        height: 0,
-        paddingBottom: '56.25%',
-      }}
-    >
-      <iframe
+    <iframe
         src={embedUrl}
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-        }}
+        style={{width: width || '100%', height: height || '100%'}}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
         title={`Videonest video ${videoId}`}
       />
-    </div>
   );
 };
 
